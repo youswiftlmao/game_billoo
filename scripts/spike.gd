de346455983af7f7c7e4e24de0b42c9822d3397f
youswiftlmao <ykhanbugatti@gmail.com>
2025-11-13 17:15:14 +0400

    FINALLY ADDED SPIKES THAT WROK CRYING RN

--- scripts/spike.gd
+++ scripts/spike.gd
@@ -1,5 +1,5 @@
 extends Sprite2D
 
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	get_tree().call_deferred("reload_scene")
+func _on_area_2d_body_entered(_body: Node2D) -> void:
+	get_tree().call_deferred("reload_current_scene")
