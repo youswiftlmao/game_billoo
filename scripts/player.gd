a617cccfb44455f27b70d764721b99759e0f9a24
youswiftlmao <ykhanbugatti@gmail.com>
2025-11-14 18:11:26 +0400

    did  anims for char

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -320.0
@@ -37,12 +38,28 @@ func _physics_process(delta: float) -> void:
 		
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	#gets input direction: -1,0,1
+	var direction := Input.get_axis("move_left", "move_right")
+	#flips sprite
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+		
+	#play animation
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("default")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("workingjump")
+	
 	if can_move == false:
 		return
 	else:
