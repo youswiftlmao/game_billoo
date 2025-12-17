e71413d22037a0945e8a9781ddb1c0c8d502dede
youswiftlmao <ykhanbugatti@gmail.com>
2025-12-17 19:46:30 +0400

    ADDED COMBAT SYSTEM WITH 1 TYPE OF ENEMY RN WOOOO

--- scripts/bat.gd
+++ scripts/bat.gd
@@ -2,7 +2,13 @@ extends CharacterBody2D
 var speed = 35
 var playerchase = false
 var player = null
+var  health = 50
+var player_inattack_zone = false
+var can_take_damage = true
+
 func _physics_process(delta: float) -> void:
+	deal_with_damage()
+	
 	if playerchase:
 		position += (player.position - position)/speed
 		
@@ -22,3 +28,31 @@ func _on_detection_body_exited(body: Node2D) -> void:
 	playerchase = false
 func enemy():
 	pass
+
+
+
+func _on_bhitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_inattack_zone = true
+	
+
+
+
+func _on_bhitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_inattack_zone = false
+		
+func deal_with_damage():
+	if player_inattack_zone and Global.player_current_attack == true:
+		if  can_take_damage == true:
+			$TAKEDAMAGECD.start()
+			can_take_damage = false
+			health = health - 20
+			print("bat got slimed = ", health)
+			if health <= 0:
+				self.queue_free()
+
+
+func _on_takedamagecd_timeout() -> void:
+	can_take_damage = true
+	
--- scripts/player.gd
+++ scripts/player.gd
@@ -10,6 +10,8 @@ var e_attackCD =  true
 var HP = 100
 var player_alive = true 
 
+var attack_ip = false
+
 
 var took_damage = false
 var can_move = true
@@ -28,24 +30,19 @@ func respawn():
 	
 func _physics_process(delta: float) -> void:
 	enemy_attack()
+	attack()
 	
 	if HP <= 0 :
 		player_alive = false
 		HP = 0
 		print("player has been eliminated")
-		self.queue_free()
+		self.queue_free() 
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	
-	for i in get_slide_collision_count():
-		var  collision = get_slide_collision(i)
-		
-		if collision.get_collider().name == "spiketiles":
-			if took_damage == false:
-				took_damage = true
-				respawn() #respawn the player?
+
 				
 			
 			
@@ -61,18 +58,21 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("move_left", "move_right")
 	#flips sprite
 	if direction > 0:
-		animated_sprite.flip_h = false
+			animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
 		
 	#play animation
 	if is_on_floor():
 		if direction == 0:
-			animated_sprite.play("default")
+			if attack_ip == false:
+				animated_sprite.play("default")
 		else:
-			animated_sprite.play("run")
+			if attack_ip == false:
+				animated_sprite.play("run")
 	else:
-		animated_sprite.play("workingjump")
+		if attack_ip == false:
+			animated_sprite.play("workingjump")
 	
 	if can_move == false:
 		return
@@ -107,3 +107,17 @@ func enemy_attack():
 
 func _on_attack_cd_timeout() -> void:
 	e_attackCD = true
+func attack():
+	
+	
+	if Input.is_action_just_pressed("attack"):
+		Global.player_current_attack = true
+		attack_ip = true
+		$AnimatedSprite2D.play("attack")
+		$deal_attack_timer.start()
+
+
+func _on_deal_attack_timer_timeout() -> void:
+	$deal_attack_timer.stop()
+	Global.player_current_attack =  false
+	attack_ip = false
