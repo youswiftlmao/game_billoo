75dfb5072d94137fe690098751a70d80644df987
youswiftlmao <ykhanbugatti@gmail.com>
2025-12-26 18:14:54 +0400

    bratty sister made me loose al my work and cutof rhw lifhrs im writing this is in darkness. anywasys completed 1st lvl working on 2nd and poslishing gameplatY

---
+++ scripts/nextlevel.gd
@@ -0,0 +1,19 @@
+extends AnimatedSprite2D
+
+var triggered := false  # prevent multiple triggers
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if triggered:
+		return
+	if body.has_method("player"):
+		triggered = true
+		# Use a short timer to delay the level change
+		var timer = Timer.new()
+		timer.wait_time = 1.0  # 1 second delay
+		timer.one_shot = true
+		add_child(timer)
+		timer.start()
+		timer.connect("timeout", Callable(self, "_change_level"))
+
+func _change_level() -> void:
+	get_tree().change_scene_to_file("res://levels/level_two.tscn")
