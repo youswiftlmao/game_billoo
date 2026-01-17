292659998c8426673df05bb46e7fd6c6abbbb560
youswiftlmao <ykhanbugatti@gmail.com>
2026-01-17 17:50:57 +0400

    biiiiigg commit finished lvl 6 game play and troll parts added enemies aznd healers to all current level working on beautifiying background andencountered a henious bug makinng mt spend hours the animations were mixed up and troll pushers/droppers just didnt stay in place

--- scripts/pusher.gd
+++ scripts/pusher.gd
@@ -12,4 +12,4 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 		$AnimationPlayer.play("PUSHER")
 
 		# Disable collision safely AFTER physics step
-		$Area2D/CollisionShape2D2.set_deferred("disabled", true)
+		$Area2D/CollisionShape2D2.set_deferred("disabled", true) 
